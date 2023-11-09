bd1d0d343eb654f8d004e5bc9a3fe0c470d12f13
Eugene-Guillermo <117149126+Eugene-Guillermo@users.noreply.github.com>
2023-11-09 20:30:41 +0800

    Animation Changes
    Changed the animation to progress sequentially without utilizing a next button and changed the labels block

--- CacheSimulation/src/MainFrame.java
+++ CacheSimulation/src/MainFrame.java
@@ -6,15 +6,16 @@ import java.awt.event.ActionListener;
 class MainFrame extends JFrame
 {
     // Arrays of JLabels to represent the blocks in the cache and main memory
-    public JLabel[] cacheBlocks, mainMemory;
+    public BlockLabel[] cacheBlocks, mainMemory;
     // JPanels used
     public JPanel northPanel, westPanel, cachePanel, eastPanel, mainMemoryPanel, southPanel, centerPanel;
     // JLabels used
-    public JLabel titleLabel, animatedLabel;
+    public JLabel titleLabel;
+    public BlockLabel animatedLabel;
     // JScrollPane for scrolling feature
     public JScrollPane cacheScroll, mainMemoryScroll;
     // JButtons for manipulating the steps
-    public JButton nextButton, skipButton;
+    public JButton playButton, skipButton, speedUpButton;
     // Amount of milliseconds in the animation
     public int PLAY_TIME = 1000;
     // Time for animation
@@ -78,15 +79,11 @@ class MainFrame extends JFrame
         cacheScroll.getVerticalScrollBar().setUnitIncrement(16);
 
         // Initializing the JLabels signifying the cache blocks
-        cacheBlocks = new JLabel[model.cache.nBlock];
+        cacheBlocks = new BlockLabel[model.cache.nBlock];
         for (int i = 0; i < model.cache.nBlock; i++)
         {
             // Initially set the JLabels to empty
-            cacheBlocks[i] = new JLabel();
-            cacheBlocks[i].setText(">Empty<");
-            cacheBlocks[i].setOpaque(true);
-            cacheBlocks[i].setFont(new Font("Arial", Font.BOLD, 16));
-            cacheBlocks[i].setAlignmentX(JLabel.CENTER_ALIGNMENT);
+            cacheBlocks[i] = new BlockLabel(null);
             cachePanel.add(cacheBlocks[i]);
         }
 
@@ -107,14 +104,10 @@ class MainFrame extends JFrame
         mainMemoryScroll.getVerticalScrollBar().setUnitIncrement(16);
 
         // Initializing the JLabels to signify the blocks to be transferred in the main memory
-        mainMemory = new JLabel[model.mainMemory.nArray.length];
+        mainMemory = new BlockLabel[model.mainMemory.nArray.length];
         for (int i = 0; i < model.mainMemory.nArray.length; i++)
         {
-            mainMemory[i] = new JLabel();
-            mainMemory[i].setText(String.valueOf(model.mainMemory.nArray[i].nNum));
-            mainMemory[i].setOpaque(true);
-            mainMemory[i].setFont(new Font("Arial", Font.BOLD, 16));
-            mainMemory[i].setAlignmentX(JLabel.CENTER_ALIGNMENT);
+            mainMemory[i] = new BlockLabel(model.mainMemory.nArray[i].nNum);
             mainMemoryPanel.add(mainMemory[i]);
         }
 
@@ -126,30 +119,48 @@ class MainFrame extends JFrame
     // Initialize the south panel
     public void initializeSouth()
     {
-        southPanel = new JPanel(new BorderLayout());
+        southPanel = new JPanel(new FlowLayout());
 
-        // Add a next button
-        nextButton = new JButton();
-        nextButton.setText("Next");
+        // Add a play button
+        playButton = new JButton();
+        playButton.setText("▶");
 
-        nextButton.addActionListener(new ActionListener() {
+        playButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                // Call the replace cache function
-                replaceCache(false);
+                // Disable play button
+                playButton.setEnabled(false);
 
-                // Disable both buttons if it is the last block already
-                if(model.mainMemory.nCurr == model.mainMemory.nArray.length)
-                {
-                    nextButton.setEnabled(false);
-                    skipButton.setEnabled(false);
-                }
+                Timer timer = new Timer(PLAY_TIME + 150, new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+
+                        // Call the replace cache function
+                        try
+                        {
+                            replaceCache(false);
+                        } catch (Exception exception)
+                        {
+                            skipButton.setEnabled(false);
+                            ((Timer)(e.getSource())).stop();
+                        }
+
+                        // Disable both buttons if it is the last block already
+                        if(model.mainMemory.nCurr == model.mainMemory.nArray.length)
+                        {
+                            skipButton.setEnabled(false);
+                            ((Timer)(e.getSource())).stop();
+                        }
+                    }
+                });
+                startTime = System.currentTimeMillis();
+                timer.start();
             }
         });
 
         // Add a skip button
         skipButton = new JButton();
-        skipButton.setText("Skip");
+        skipButton.setText("▶▶|");
 
         skipButton.addActionListener(new ActionListener() {
             @Override
@@ -159,13 +170,27 @@ class MainFrame extends JFrame
                     replaceCache(true);
 
                 // Disable both buttons since there is no more blocks to be transferred
-                nextButton.setEnabled(false);
+                playButton.setEnabled(false);
                 skipButton.setEnabled(false);
+                speedUpButton.setEnabled(false);
             }
         });
 
-        southPanel.add(nextButton, BorderLayout.WEST);
-        southPanel.add(skipButton, BorderLayout.EAST);
+        speedUpButton = new JButton();
+        speedUpButton.setText("▶▶");
+
+        speedUpButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PLAY_TIME -= 250;
+                if (PLAY_TIME == 250)
+                    speedUpButton.setEnabled(false);
+            }
+        });
+
+        southPanel.add(playButton);
+        southPanel.add(speedUpButton);
+        southPanel.add(skipButton);
 
         this.add(southPanel,BorderLayout.SOUTH);
     }
@@ -175,40 +200,54 @@ class MainFrame extends JFrame
     {
         // Get the position in the cache to be replaced
         int num = model.replaceCache();
-        // Replace the number inside the JLabel inside the cache
-        cacheBlocks[num].setText(String.valueOf(model.cache.nArray[num]));
-        // Change all the background of the blocks
-        for (int i = 0; i < cacheBlocks.length; i++)
-            cacheBlocks[i].setBackground(Color.WHITE);
-        // Highlight the block from the main memory moved or found in the cache
-        cacheBlocks[num].setBackground(Color.YELLOW);
-
-        // Change the current scroll position to the latest cache block
-        cacheScroll.getViewport().setViewPosition(cacheBlocks[num].getLocation());
 
         // Get the current block to be moved into the cache
         int num2 = model.mainMemory.nCurr;
-        // If it is not beyond the last main memory then highlight it yellow
-        if (!(num2 == mainMemory.length))
-            mainMemory[num2].setBackground(Color.YELLOW);
-        // If the last one is a hit then highlight it green
-        if (model.mainMemory.nArray[num2 - 1].cHit.equals("Hit"))
-            mainMemory[num2 - 1].setBackground(Color.GREEN);
-        // If the last one is a miss then highlight it red
-        else if (model.mainMemory.nArray[num2 - 1].cHit.equals("Miss"))
-            mainMemory[num2 - 1].setBackground(Color.RED);
+
+        // Change the current scroll position to the second to the latest cache block
+        if (num > 1)
+            if (num % 4 == 0)
+                cacheScroll.getViewport().setViewPosition(cacheBlocks[num - 1].getLocation());
 
         // If it is not yet at the end then change the current scroll position to the latest main memory block
+        if (!(num2 == mainMemory.length) && num2 != 1)
+            mainMemoryScroll.getViewport().setViewPosition(mainMemory[num2 - 1].getLocation());
+
+        // Change all the background of the blocks
+        for (int i = 0; i < cacheBlocks.length; i++)
+        {
+            cacheBlocks[i].color = "Silver";
+            cacheBlocks[i].update();
+        }
+
+        // Highlight the block from the main memory moved or found in the cache
+        cacheBlocks[num].color = "Yellow";
+
+        cacheBlocks[num].update();
+
+        // If it is not beyond the last main memory then highlight it yellow
         if (!(num2 == mainMemory.length))
-            mainMemoryScroll.getViewport().setViewPosition(mainMemory[num].getLocation());
+            mainMemory[num2].color = "Yellow";
+        // If the last one is a hit then highlight it green
+        if (model.mainMemory.nArray[num2 - 1].cHit.equals("Hit"))
+            mainMemory[num2 - 1].color = "Lime";
+        // If the last one is a miss then highlight it red
+        else if (model.mainMemory.nArray[num2 - 1].cHit.equals("Miss"))
+            mainMemory[num2 - 1].color = "Red";
+
+        if (num2 != model.mainMemory.nArray.length)
+            mainMemory[num2].update();
+        mainMemory[num2 - 1].update();
+
+        // Replace the number inside the JLabel inside the cache
+        cacheBlocks[num].number = model.cache.nArray[num];
 
         // If the skip button is not pressed and it isn't at the last main memory block
         if (!(num2 == mainMemory.length) && !(isSkip))
         {
             // Temporarily disable the buttons and animate
-            skipButton.setEnabled(false);
-            nextButton.setEnabled(false);
-            animateLabel(mainMemory[num2], cacheBlocks[num]);
+            playButton.setEnabled(false);
+            animateLabel(mainMemory[num2 - 1], cacheBlocks[num]);
         }
 
         // Refresh the view
@@ -218,14 +257,12 @@ class MainFrame extends JFrame
 
     // Reference to base the animation on
     //https://stackoverflow.com/questions/16577415/jlabel-move-to-coordinate
-    public void animateLabel(JLabel startLabel, JLabel destinationLabel)
+    public void animateLabel(BlockLabel startLabel, BlockLabel destinationLabel)
     {
         // Create the animated label
-        animatedLabel = new JLabel();
-        animatedLabel.setText(destinationLabel.getText());
-        animatedLabel.setOpaque(true);
-        animatedLabel.setBackground(Color.CYAN);
-        animatedLabel.setFont(new Font("Arial", Font.BOLD, 16));
+        animatedLabel = new BlockLabel(destinationLabel.number);
+        animatedLabel.color = "Aqua";
+        animatedLabel.update();
         animatedLabel.setSize(animatedLabel.getPreferredSize());
 
         // Add the animated label and have it be invisible for now
@@ -234,6 +271,7 @@ class MainFrame extends JFrame
 
         // Use the timer to repeatedly change the postition with a delay
         Timer timer = new Timer(50, new ActionListener() {
+
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Change it to be visible and refresh its view
@@ -245,6 +283,10 @@ class MainFrame extends JFrame
                 double x1 = startLabel.getLocationOnScreen().getX(), y1 = startLabel.getLocationOnScreen().getY();
                 double x2 = destinationLabel.getLocationOnScreen().getX(), y2 = destinationLabel.getLocationOnScreen().getY();
 
+                double offsetX = centerPanel.getLocationOnScreen().getX();
+                double offsetY = centerPanel.getLocationOnScreen().getY();
+
+
                 // Get the time and progress
                 long duration = System.currentTimeMillis() - startTime;
                 float progress = (float) duration / (float) PLAY_TIME;
@@ -255,7 +297,6 @@ class MainFrame extends JFrame
 
                     // Re-enable the buttons
                     skipButton.setEnabled(true);
-                    nextButton.setEnabled(true);
 
                     // Remove the animated Label and refresh the view
                     centerPanel.remove(animatedLabel);
@@ -270,9 +311,8 @@ class MainFrame extends JFrame
                 double x = x1 + (int) Math.round((x2 - x1) * progress);
                 double y = y1 + (int) Math.round((y2 - y1) * progress);
 
-                // Remove the position of the center panel
-                x -= centerPanel.getLocationOnScreen().getX();
-                y -= centerPanel.getLocationOnScreen().getY();
+                x -= offsetX;
+                y -= offsetY;
 
                 // Set the location
                 animatedLabel.setLocation((int) x, (int) y);
@@ -281,5 +321,24 @@ class MainFrame extends JFrame
         // Start the timer loop
         startTime = System.currentTimeMillis();
         timer.start();
+    }
+}
+class BlockLabel extends JLabel {
+    Integer number;
+    String color = "Silver";
+
+    public BlockLabel(Integer number)
+    {
+        this.number = number;
+        super.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+
+        this.update();
+    }
+    public void update()
+    {
+        if (this.number == null)
+            super.setText("<html><p style=\"font-size: 12px; font-weight: bold; background-color:" + this.color + "; border: 1px; border-radius: 2px; border-style: solid; border-color: black; padding: 2px 8px; margin: 2px; text-shadow: 1px;\">Empty</p></html>");
+        else
+            super.setText("<html><p style=\"font-size: 12px; font-weight: bold; background-color:" + this.color + "; border: 1px; border-radius: 2px; border-style: solid; border-color: black;  padding: 2px 8px; margin: 2px; text-shadow: 1px;\">"+ this.number +"</p></html>");
     }
 }
